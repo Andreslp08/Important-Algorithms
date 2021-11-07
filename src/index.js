1c614bbadff0f3c7a8923117671d00630728caf6
Andrés López <andreslp08@hotmail.com>
2021-11-07 14:43:30 -0500

    bubble sort added

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@
 
 const {binarySearch} = require('./search-algorithms/binary-search');
 const {generateNumberArray} = require('./utils/utils');
+const {bubbleSort} = require('./sort/bubble-sort');
 
 const app = ()=>{
   //------ BINARY SEARCH -------------
-  const arr = generateNumberArray(1,99999);
-  console.time();
-  const numberToFind = binarySearch(arr, 768);
-  console.log(numberToFind);
-  console.timeEnd();
+  // const arr = generateNumberArray(1,99999);
+  // console.time();
+  // const numberToFind = binarySearch(arr, 768);
+  // console.log(numberToFind);
+  // console.timeEnd();
+  //------ BUBBLE SORT -------------
+  const arr = [5,10,20,3,6,1 ,1];
+  const sort = bubbleSort(arr);
+  console.log(sort)
+
+  
 }
 
 
---
+++ src/sort/bubble-sort.js
@@ -0,0 +1,19 @@
+
+
+const bubbleSort = (arr = []) =>{
+    if(Array.isArray(arr) == false){return false};
+    for (let i = 0; i < arr.length; i++) {
+       for (let j = 0; j < arr.length; j++) {
+           if( arr[j] > arr[j+1]  ){
+               const temp = arr[j];
+               arr[j] = arr[j+1];
+               arr[j+1] = temp;
+           }
+       }
+    }
+    return arr;
+}
+
+module.exports = {
+    bubbleSort
+}
